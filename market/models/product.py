15a703da1981c82f796c05e63ba4fca04c958bbb
Abddev-09 <abddev09@gmail.com>
2025-11-06 11:10:14 +0500

    hahahha

--- market/models/product.py
+++ market/models/product.py
@@ -9,5 +9,10 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     image = models.ImageField(upload_to="products/",null=True,blank=True)
 
+
+    def delete(self, *args, **kwargs):
+        self.is_active = False
+        self.save()
+
     def __str__(self):
         return self.name
