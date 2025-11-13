b7bda1064c106740cd03c34415419b46325818bb
Abddev-09 <abddev09@gmail.com>
2025-11-13 17:23:17 +0500

    settings

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from .views import (
     UserViews,
     ImportStudents,
     ChangePasswordView,
-    ExportUsersView, TeacherView
+    TeacherView
 )
 
 # 🔹 Router faqat ModelViewSet uchun
@@ -17,7 +17,6 @@ urlpatterns = [
     path('login/', AuthViews.as_view(), name='login'),
     path('students/import/', ImportStudents.as_view(), name='import-students'),
     path('reset-password/', ChangePasswordView.as_view(), name='user-reset-password'),
-    path('export-users/', ExportUsersView.as_view(), name='export-users-excel'),
     path("my-students/",TeacherView.as_view(),name="my-students"),
     # router url-larni qo‘shamiz
     path('', include(router.urls)),
