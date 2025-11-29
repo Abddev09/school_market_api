e9968fecdb362c0ae0f26df1074abbc3ed1a9be5
Abdulloh Ubaydullayev <abddev09@gmail.com>
2025-11-29 09:13:08 +0500

    Change Cart product ForeignKey to use CASCADE

--- market/models/cart.py
+++ market/models/cart.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Cart(models.Model):
-    product = models.ForeignKey('market.Product',on_delete=models.DO_NOTHING,null=True)
+    product = models.ForeignKey('market.Product',on_delete=models.CASCADE,null=True)
     student = models.ForeignKey('user.User',on_delete=models.SET_NULL,null=True,related_name="cart")
 
     def __str__(self):
